e3f228a72792ff550793fe6f3b2dfa591254bfe7
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2019-02-02 04:32:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 // Code your solution in this file
 function findMatching(drivers, string) {
-  drivers.filter(function (s)) {
-    return s == string;
-  }
+  drivers.filter(function (s) {
+     s == string})
 }
